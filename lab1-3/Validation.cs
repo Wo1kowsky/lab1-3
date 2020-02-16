13b889cb3f1a669ed46969f2308955feecccdab4
Wo1kowsky <43152077+Wo1kowsky@users.noreply.github.com>
2020-02-16 16:52:41 +0300

    fixed validation

--- lab1-3/Validation.cs
+++ lab1-3/Validation.cs
@@ -4,12 +4,12 @@ static public class Validation
 {
     static public bool IsLengthValid(string input)
     {
-        return input.Length == 4 ? true : false;
+        return input.Length == 4; ;
     }
 
     static public bool IsPositiveInteger(string input)
     {
         int number;
-        return Int32.TryParse(input, out number) && number > 0 ? true : false;
+        return Int32.TryParse(input, out number) && number > 0;
     }
 }
