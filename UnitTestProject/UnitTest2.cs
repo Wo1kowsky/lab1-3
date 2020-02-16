63a7f1093464e4c1e20eceb74df8d08e0ec58e41
Wo1kowsky <43152077+Wo1kowsky@users.noreply.github.com>
2020-02-16 17:19:31 +0300

    isInputValid method added

---
+++ UnitTestProject/UnitTest2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lab1_3.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Validation.IsLengthValid(""), "");
+        }
+    }
+}
--- lab1-3/Validation.cs
+++ lab1-3/Validation.cs
@@ -2,12 +2,16 @@
 
 static public class Validation
 {
-    static public bool IsLengthValid(string input)
+    static public bool isInputValid(string input)
+    {
+        return IsLengthValid(input) && IsPositiveInteger(input);
+    }
+    static private bool IsLengthValid(string input)
     {
         return input.Length == 4;
     }
 
-    static public bool IsPositiveInteger(string input)
+    static private bool IsPositiveInteger(string input)
     {
         int number;
         return Int32.TryParse(input, out number) && number > 0;
