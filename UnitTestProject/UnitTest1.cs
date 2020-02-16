bc5d59d2667247d380be26d60a1a70f90147e59e
Rugged-bl <vasia.shumilov@gmail.com>
2020-02-16 16:41:05 +0300

    logic + initial test

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lab1_3.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual(Logic.FilterNumbers(
+                new List<string> { "757", "777", "7" }),
+                new List<string> { "757" }
+            );
+        }
+    }
+}
