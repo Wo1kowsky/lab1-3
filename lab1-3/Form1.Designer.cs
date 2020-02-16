c170153e69a5d271e83b1f339dad8da071c97a1d
Wo1kowsky <43152077+Wo1kowsky@users.noreply.github.com>
2020-02-16 14:36:52 +0300

    Добавьте файлы проекта.

---
+++ lab1-3/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace lab1_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num3 = new System.Windows.Forms.TextBox();
+            this.num4 = new System.Windows.Forms.TextBox();
+            this.num9 = new System.Windows.Forms.TextBox();
+            this.num5 = new System.Windows.Forms.TextBox();
+            this.num10 = new System.Windows.Forms.TextBox();
+            this.num7 = new System.Windows.Forms.TextBox();
+            this.num8 = new System.Windows.Forms.TextBox();
+            this.num6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.TextBox();
+            this.execButton = new System.Windows.Forms.Button();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 71F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 72F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 84F));
+            this.tableLayoutPanel1.Controls.Add(this.num6, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.num8, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.num7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.num10, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.num9, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.num4, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.num3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.num2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.num1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.num5, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.result, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.execButton, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.exit_button, 2, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(1, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 53.16456F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 46.83544F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 127F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(375, 267);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(3, 23);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(66, 20);
+            this.num1.TabIndex = 0;
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(151, 23);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(65, 20);
+            this.num3.TabIndex = 2;
+            this.num3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // num4
+            // 
+            this.num4.Location = new System.Drawing.Point(222, 23);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(66, 20);
+            this.num4.TabIndex = 3;
+            // 
+            // num9
+            // 
+            this.num9.Location = new System.Drawing.Point(222, 54);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(66, 20);
+            this.num9.TabIndex = 4;
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(294, 23);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(66, 20);
+            this.num5.TabIndex = 5;
+            // 
+            // num10
+            // 
+            this.num10.Location = new System.Drawing.Point(294, 54);
+            this.num10.Name = "num10";
+            this.num10.Size = new System.Drawing.Size(66, 20);
+            this.num10.TabIndex = 6;
+            // 
+            // num7
+            // 
+            this.num7.Location = new System.Drawing.Point(77, 54);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(66, 20);
+            this.num7.TabIndex = 7;
+            // 
+            // num8
+            // 
+            this.num8.Location = new System.Drawing.Point(151, 54);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(65, 20);
+            this.num8.TabIndex = 8;
+            // 
+            // num6
+            // 
+            this.num6.Location = new System.Drawing.Point(3, 54);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(64, 20);
+            this.num6.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 5);
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Введите 10 положительных четырехзначных целых чисел";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(77, 23);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(64, 20);
+            this.num2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label2, 4);
+            this.label2.Location = new System.Drawing.Point(3, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(283, 26);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Количество чисел с суммой цифр кратной 7 и цифры - арифметическая прогрессия";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(3, 114);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(64, 20);
+            this.result.TabIndex = 12;
+            // 
+            // execButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.execButton, 2);
+            this.execButton.Location = new System.Drawing.Point(3, 142);
+            this.execButton.Name = "execButton";
+            this.execButton.Size = new System.Drawing.Size(75, 23);
+            this.execButton.TabIndex = 13;
+            this.execButton.Text = "Выполнить";
+            this.execButton.UseVisualStyleBackColor = true;
+            // 
+            // exit_button
+            // 
+            this.exit_button.Location = new System.Drawing.Point(151, 142);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(65, 23);
+            this.exit_button.TabIndex = 14;
+            this.exit_button.Text = "Выход";
+            this.exit_button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 308);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num3;
+        private System.Windows.Forms.TextBox num6;
+        private System.Windows.Forms.TextBox num8;
+        private System.Windows.Forms.TextBox num7;
+        private System.Windows.Forms.TextBox num10;
+        private System.Windows.Forms.TextBox num9;
+        private System.Windows.Forms.TextBox num4;
+        private System.Windows.Forms.TextBox num5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button execButton;
+        private System.Windows.Forms.Button exit_button;
+    }
+}
+
