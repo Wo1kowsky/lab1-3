075598f7bea2711452fea56da7ac7d36e9b5e5be
Wo1kowsky <43152077+Wo1kowsky@users.noreply.github.com>
2020-02-18 19:43:50 +0300

    added integration test

---
+++ UnitTestProject/IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using lab1_3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject.IntegrationTest
+{
+    [TestClass]
+    public class IntegrationTest
+    {
+        [TestMethod]
+        public void CheckFilterNumbersFull()
+        {
+            List<string> list = new List<string>() { "8888", "7777", "7776", "1234", "1237" };
+            Assert.AreEqual(list.TrueForAll(
+                Validation.isInputValid), 
+                true);
+            CollectionAssert.AreEqual(
+                list,
+                new List<string> { "7776", "1237" }
+            );
+        }
+    }
+}
